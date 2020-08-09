5e7017e0fc24183d54904b3c58a881b618480402
Do Trung Duc <duc010298@gmail.com>
2020-08-09 21:01:08 +0700

    up

--- Note.sh
+++ Note.sh
@@ -9,3 +9,8 @@ docker rm -f $(docker ps -a -q)
 
 # Remove all image
 docker rmi $(docker images -q)
+
+# Install docker compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)"  -o /usr/local/bin/docker-compose
+sudo mv /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo chmod +x /usr/bin/docker-compose
